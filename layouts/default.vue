620840c0e2670cc377482f54e8535d1c05b77e48
4m1t0 <fum1t0.eng@gmail.com>
2019-12-22 22:51:11 +0900

    不要な修正を削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,29 +16,6 @@ import Navibar from '@/components/Navibar'
 export default {
   components: {
     Navibar
-  },
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: '合唱団ばすけ'
-    }
   }
 }
 </script>
